4a93294654b05fd4464de2190ea827abad5beac1
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-12-11 00:33:13 +0100

    home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -61,15 +61,15 @@ const SkilledLearning = () => {
           </div> */}
         </div>
         <div className="flex justify-start items-end my-5 xl:my-20">
-        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-1 lg:grid-cols-12">
         <div class="mr-auto place-self-center lg:col-span-7 pl-12 pt-15 ">
-            <h1 class=" max-w-2xl sm:mt-20 xl:mt-0 mb-4 text-5xl font-extrabold tracking-tight leading-none md:text-5xl  xl:text-6xl text-white">You have the will to create the life you aspire for</h1>
-            <p class="max-w-2xl sm:mt-12 lg:mt-0 mb-6 font-light xl:py-2 xs:py-9 text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Always remember to labour under the correct knowledge</p>
+            <h1 class=" max-w-2xl sm:mt-20 xl:mt-0 mb-4 text-5xl lg:py-3 font-extrabold tracking-tight leading-none md:text-5xl  xl:text-6xl text-white">You have the will to create the life you aspire for</h1>
+            <p class="max-w-2xl sm:mt-12 lg:mt-0 mb-6 font-light xl:py-2 xs:py-9 text-gray-400 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Always remember to labour under the correct knowledge</p>
             <a href="#" class=" sm:mt-5 lg:mt-0 inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-yellow-300 rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 Get started
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
-            <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center bg-green-500 dark:text-black border hover:text-black border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-yellow-400 dark:focus:ring-gray-800">
+            <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center bg-yellow-200 border hover:text-black border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 text-gray-700 dark:border-gray-700 dark:hover:bg-yellow-400 dark:focus:ring-gray-800">
                 Register Now
             </a> 
         </div>
@@ -93,9 +93,9 @@ const SkilledLearning = () => {
           </div>
         </div>
         <div className="pt-56 md:pt-0 grid sm:grid-cols-2 lg:grid-cols-3 gap-20 my-24 lg:my-32 place-items-center sm:place-items-stretch">
-          {/* <div className="hover:drop-shadow-2xl w-full bg-gradient-to-r from-pink-500 to-red-600 sm:max-w-xs rounded-xl px-10 py-5 text-white text-xl font-extrabold leading-8 shadow-xl">
+          <div className="hover:drop-shadow-2xl w-screen bg-gradient-to-r  from-pink-500 to-red-600 sm:max-w-xs rounded-xl px-10 py-5 text-white text-xl font-extrabold leading-8 shadow-xl">
             Check out our most popular courses!
-          </div> */}
+          </div>
           {/* {CardJson.map((data, index) => {
             return (
               <div
